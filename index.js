fa16f2b9bc96b8c4ed45dd3b730cb7fba9ad6e25
bebawymaged <102050541+bebawymaged@users.noreply.github.com>
2023-08-02 19:32:14 -0700

    updated port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
-const port = process.env.PORT || 22;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
